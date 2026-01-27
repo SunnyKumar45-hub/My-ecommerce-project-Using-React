488555dd624d3a516242ad3611d1747d32ef3ccd
SunnyKumar45-hub <renubisa389@gmail.com>
2026-01-28 03:00:13 +0530

    Remove all unnecessary folders

--- src/problem-soln.js
+++
@@ -1,6 +0,0 @@
-const totalCost = (10 + 2*8);
-console.log(totalCost);
-
-const ele = document.querySelector('element');
-const ele2 = document.querySelector('element2');
-const ele3 = document.querySelector('element3');
