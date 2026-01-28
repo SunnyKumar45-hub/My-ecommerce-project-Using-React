bacca67a0efc99756488341a76dfcc34ac9a7ce0
Sunny kumar <qufranetfe02@gmail.com>
2026-01-29 03:03:38 +0530

    Update server.js

--- ecommerce-backend/server.js
+++ ecommerce-backend/server.js
@@ -25,7 +25,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}))
 app.use(express.json());
 
 // Serve images from the images folder
