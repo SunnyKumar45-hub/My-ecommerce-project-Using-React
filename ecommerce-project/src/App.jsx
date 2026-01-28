54779e5fcca34fc5558b0dfa7bd89dcc9bc51b89
Sunny kumar <qufranetfe02@gmail.com>
2026-01-28 23:55:30 +0530

    Update App.jsx

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
 
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<HomePage />} />
       </Routes>
     </>
   )
