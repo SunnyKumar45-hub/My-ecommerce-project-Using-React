83620f6f3c27cb14b0c53d1193f6c5caaa31f93f
Sunny kumar <qufranetfe02@gmail.com>
2026-01-29 02:55:08 +0530

    Update OrdersDetailsGrid.jsx

--- ecommerce-project/src/Pages/orders/OrdersDetailsGrid.jsx
+++ ecommerce-project/src/Pages/orders/OrdersDetailsGrid.jsx
@@ -2,28 +2,26 @@ import dayjs from 'dayjs';
 import { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { API_URL } from '../../config'; // ADD THIS
 
 export function OrdersDetailsGrid({ order, loadCart }) {
-
   const addOrderedQuantity = async (productId) => {
-    await axios.post('/api/cart-items', {
+    await axios.post(`${API_URL}/api/cart-items`, { // UPDATE THIS
       productId,
       quantity: 1
     });
     await loadCart();
   };
-
-
+  
   return (
     <div className="order-details-grid">
-
       {order.products.map((orderProduct) => {
         return (
           <Fragment key={orderProduct.product.id}>
             <div className="product-image-container">
-              <img src={orderProduct.product.image} />
+              <img src={`${API_URL}/${orderProduct.product.image}`} // UPDATE THIS
+                alt={orderProduct.product.name} />
             </div>
-
             <div className="product-details">
               <div className="product-name">
                 {orderProduct.product.name}
@@ -36,13 +34,12 @@ export function OrdersDetailsGrid({ order, loadCart }) {
               </div>
               <button className="buy-again-button button-primary"
                 onClick={() => addOrderedQuantity(orderProduct.product.id)}>
-                <img className="buy-again-icon" src="images/icons/buy-again.png" />
+                <img className="buy-again-icon" src="images/icons/buy-again.png" alt="Buy again" />
                 Add to Cart
               </button>
             </div>
-
             <div className="product-actions">
-              <Link to={`/tracking/${order.id}/${orderProduct.product.id}`}>
+              <Link to={`/tracking/${order.id}/${orderProduct.product.id}`}> {/* FIX THIS - was missing curly braces */}
                 <button className="track-package-button button-secondary">
                   Track package
                 </button>
@@ -54,4 +51,3 @@ export function OrdersDetailsGrid({ order, loadCart }) {
     </div>
   );
 }
-
