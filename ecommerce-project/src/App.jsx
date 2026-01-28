79ab47e19860d64f69c512d5cd779ffd24e4a61c
Sunny kumar <qufranetfe02@gmail.com>
2026-01-29 00:10:12 +0530

    Update App.jsx

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
+import HomePage from './Pages/HomePage';
 import Navbar from './components/Navbar';
 const App = () => {
   return (
